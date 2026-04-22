708eb3e808bc60f7a0e68d37abbeb1e96933609b
Raissa <raissa.feitosa06@gmail.com>
2026-04-22 11:38:37 -0300

    feat: resolve exercicio de classificação impar/par e positivo/negativo

---
+++ src/lacoCondicionalIf/ClassificadorNumerico.java
@@ -0,0 +1,31 @@
+package lacoCondicionalIf;
+
+import java.util.Scanner;
+
+public class ClassificadorNumerico {
+
+	public static void main(String[] args) {
+		
+		int numero;
+		Scanner scanner = new Scanner (System.in);
+		
+		//Entrada de dados
+		System.out.println("Digite um número: ");
+		numero = scanner.nextInt();
+		
+
+		// Processamento
+		if (numero >= 0 && numero % 2 == 0) {
+			System.out.println("O Número "+ numero + " é par e positivo!");
+			
+		} else if (numero < 0 && numero % 2 != 0) {
+			System.out.println("O Número "+ numero + " é impar e negativo!");
+			
+		} else if (numero < 0 && numero % 2 == 0) {
+			System.out.println("O Número "+ numero + " é par e negativo!");
+		} else {
+			System.out.println("O Número "+ numero + " é impar e positivo!");
+		}
+	}
+
+}

6cf4551c2029dbbda94c49f94b15dc29f4b53d83
Raissa <raissa.feitosa06@gmail.com>
2026-04-22 11:29:29 -0300

    feat: resolve exercicio de soma A+B

---
+++ src/lacoCondicionalIf/ComparadorSoma.java
@@ -0,0 +1,38 @@
+package lacoCondicionalIf;
+
+import java.util.Scanner;
+
+public class ComparadorSoma {
+
+	public static void main(String[] args) {
+		
+		int numeroA, numeroB, numeroC, soma;
+		Scanner scanner = new Scanner (System.in);
+		
+		//Entrada de dados
+		System.out.println("Digite o número A: ");
+		numeroA = scanner.nextInt();
+		
+		System.out.println("Digite o número B: ");
+		numeroB = scanner.nextInt();
+		
+		System.out.println("Digite o número C: ");
+		numeroC = scanner.nextInt();
+		
+		// Processamento
+		soma = numeroA + numeroB;
+		if (soma > numeroC) {
+			System.out.println("A Soma de A + B é Maior do que C");
+			
+		} else if (soma < numeroC) {
+			System.out.println("A Soma de A + B é Menor do que C");
+		}
+		
+		else {
+			System.out.println("A Soma de A + B é Igual a C");
+		}
+
+
+	}
+
+}
